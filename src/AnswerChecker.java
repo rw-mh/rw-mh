5abc6103e1e25ab0fcfc57a900db93d3c6e19fe3
rw-mh <2250961804@qq.com>
2025-03-19 13:41:09 +0800

    Delete src directory

--- src/AnswerChecker.java
+++
@@ -1,5 +0,0 @@
-public class AnswerChecker {
-    public static void check(String exerciseFile, String answerFile) {
-        // Read both files, compare answers, and write results to Grade.txt
-    }
-}
